15a8a8c744786f7d1f5dfeaf84be5f5811e7854f
zx186 <109511309+zx186@users.noreply.github.com>
2022-07-28 22:14:03 +0800

    Add files via upload

---
+++ day6作业有点名器js和ts/one.js
@@ -0,0 +1,12 @@
+//泛形 
+/**
+ * any类型，表达一个值为任意类型的时候，指定他的类型为any，
+ *
+ *
+ * new Array()  创建数组的长度
+ * .fill，填充数组的方法，将数据填充到其中，分别以不同的数据类型进行填充
+ *
+ */
+// const getArray=(value:any,times:number=5):any[]=>{
+//     return new Array(times).fill(value)
+// }
